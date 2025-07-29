c3a3df7c886ec11a24da213fbcb98323b2905a92
Nayan17107 <prajapatinayan17107@gmail.com>
2025-07-29 11:56:03 +0530

    29/07/2025

---
+++ DSA/2DArray.js
@@ -0,0 +1,30 @@
+// let arr = [
+//     [11, 22, 33],
+//     [44, 55, 66],
+//     [77, 88, 99]
+// ]
+
+// console.log(arr);
+// console.log(arr[1][2]);
+
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//         console.log(arr[i][j]);
+//     }
+// }
+
+
+// for (let i = 0; i < arr.length; i++) {
+//     let Matrix = "";
+//     for (let j = 0; j < arr[i].length; j++) {
+//         Matrix += arr[i][j] + " ";
+//     }
+//     console.log(Matrix);
+// }
+
+// arr.pop();
+// arr.push([111,222,333]);
+// arr[0].push(99)
+// arr[1][0] = 0
+
+// console.log(arr);
---
+++ DSA/linear-search.js
@@ -0,0 +1,37 @@
+// let arr = [11,22,33,44,55,66,77,88,99,2,7,23,6,32,66,56,11,11];
+
+// key = 11;
+
+// let linearsearch = (arr,key) => {
+//     for(let i = 0; i < arr.length; i++){
+//         if(arr[i] == key){
+//             return i;
+//         }
+//     }
+//     return -1;
+// }
+// let ans = linearsearch(arr, key);
+
+// if(ans == -1){
+//     console.log("Element is not Found");
+// }else{
+//     console.log("Element is Found at :-", ans);
+// }
+
+
+
+// let arr = [11, 22, 33, 44, 55, 66, 77, 88, 99, 2, 7, 23, 6, 32, 66, 56, 11, 11];
+// key = 11;
+// let index = [];
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (arr[i] == key) {
+//         index.push(i);
+//     }
+// }
+
+// if (index.length == 0) {
+//     console.log("Element is not Found");
+// } else {
+//     console.log("Element is Found at :-", index);
+// }
