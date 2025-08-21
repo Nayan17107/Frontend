dcd32e9545f720e4954036e79dc4a0c6a19f371a
Nayan17107 <prajapatinayan17107@gmail.com>
2025-08-21 11:26:31 +0530

    21/08/2025

---
+++ DSA/Set & Map.js
@@ -0,0 +1,40 @@
+// Set Mathods => Used to store unique value
+
+
+// let data = new Set([11,22,33,44,22,55,11])
+
+// console.log(data);
+
+// data.add(66);                                        // Add Value in a set   
+// data.add(77);                                        // Add Value in a set
+// console.log(data.has(11));                           // Checks that value is included or not
+// console.log(data.has(66));                           // Checks that value is included or not
+// data.delete(11);                                     // Delete Seprate value
+// data.clear();                                        // Clear the Whole Set
+// console.log(data.keys());                            // Return the Keys of set
+// console.log(data.values());                          // Return the Values of set
+// data.forEach(n => console.log(n));                   // Used to print Single set
+
+// console.log(data);
+
+
+// Map => Used to store unique value But in Object Way like with pair of key and value
+
+// let data = new Map([
+//     ["Nayan" , "Prajapati"],
+//     ["Meet" , "Kotadiya"],
+//     ["Trapasiya" , "Meetbhai"],
+//     ["Darshik" , "Shekhada"],
+//     ["luv" , "Dhameliya"]
+// ])
+
+// data.set("Krish" , "Pajiyawala");                    // Add Data in the map
+// console.log(data.get("Nayan"));                      // Return the value of given Array
+// data.delete("Trapasiya" , "Meetbhai");               // Delete Seprate key and value
+// console.log(data.keys());                            // Return only key of map
+// console.log(data.values());                          // Return only values of map
+// data.clear();                                        // Clear whole map
+// console.log(data.has("Nayan" , "Prajapati"));        // Checks it is included or not
+// data.forEach((n , i) => console.log(i , n));         // Used to print Single map
+
+// console.log(data);
