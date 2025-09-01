b18b70e1d66902cbc28bf3ed73de2817ea9ac008
Nayan17107 <prajapatinayan17107@gmail.com>
2025-09-01 10:20:20 +0530

    01/09/2025

--- DSA/Queue.js
+++ DSA/Queue.js
@@ -31,7 +31,9 @@
 // queue.printQueue();  
 
 
+
 // Doing Stack Task With Using Queue
+
 
 
 // class Queue{
@@ -64,3 +66,44 @@
 // Que.delete();
 
 // console.log(Que);
+
+
+
+// Doing Queue Using Stack
+
+
+class Queue{
+    constructor(){
+        this.s1 = []
+        this.s2 = []
+    }
+
+    Add(ele){
+        this.s1.push(ele)
+    }
+
+    Delete(){
+        while(this.s1.length > 1){
+            this.s2.push(this.s1.pop());
+        }
+        this.s1.pop();
+
+        while(this.s2.length > 0){
+            this.s1.push(this.s2.pop());
+        }
+    }
+}
+
+let Que = new Queue();
+
+Que.Add(10);
+Que.Add(20);
+Que.Add(30);
+Que.Add(40);
+Que.Add(50);
+
+console.log(Que);
+
+Que.Delete()
+
+console.log(Que);
