80ab472330aac2c9f879f0311a6a5149fb446cde
Nayan17107 <prajapatinayan17107@gmail.com>
2025-09-01 10:44:10 +0530

    01/09/2025

--- DSA/Bineary-Search.js
+++ DSA/Bineary-Search.js
@@ -1,27 +1,26 @@
 // Binary search
 
-let myArray = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
-let myTarget = 19 ;
-let left = 0;
-let right = myArray.length -1;
+// let myArray = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19];
+// let myTarget = 19 ;
+// let left = 0;
+// let right = myArray.length -1;
 
-while(true){
-    console.log("array = ", left, right);
-    if(right < 0 || left >= myArray.length){
+// while(true){
+//     if(right < 0 || left >= myArray.length){
 
-        console.log("value does not found");
-        break;
-    }
+//         console.log("value does not found");
+//         break;
+//     }
 
-    let mid = Math.floor((left + right) / 2);
+//     let mid = Math.floor((left + right) / 2);
 
-    if(myArray[mid] == myTarget){
-        console.log("Target value is ",mid);
-        break;
-    }
-    if(myArray[mid]< myTarget){
-        left = mid +1;
-    }else{
-        right = mid -1;
-    }
-}
+//     if(myArray[mid] == myTarget){
+//         console.log("Target value is ",mid);
+//         break;
+//     }
+//     if(myArray[mid]< myTarget){
+//         left = mid +1;
+//     }else{
+//         right = mid -1;
+//     }
+// }
