83ceedb289acc56ebe1d250b847536df1600619d
Nayan17107 <prajapatinayan17107@gmail.com>
2025-09-03 10:20:43 +0530

    03/09/2025

---
+++ DSA/Bound.js
@@ -0,0 +1,47 @@
+// Lower Bound
+
+
+// let arr = [1, 2, 4, 4, 4, 5, 7, 9];
+// let target = 7;
+
+// function lowerBound(arr, target) {
+//     let low = 0, high = arr.length;
+
+//     while (low < high) {
+//         let mid = Math.floor((low + high) / 2);
+
+//         if (arr[mid] < target) {
+//             low = mid + 1;
+//         } else {
+//             high = mid;
+//         }
+//     }
+//     return low;
+// }
+
+// console.log("Lower Bound:", lowerBound(arr, target)); 
+
+
+
+// Upper Bound
+
+
+function upperBound(arr, target) {
+    let low = 0, high = arr.length;
+
+    while (low < high) {
+        let mid = Math.floor((low + high) / 2);
+
+        if (arr[mid] <= target) {
+            low = mid + 1;
+        } else {
+            high = mid;
+        }
+    }
+    return low;
+}
+
+let arr = [1, 2, 4, 4, 4, 5, 7, 9];
+let target = 4;
+
+console.log("Upper Bound:", upperBound(arr, target));
