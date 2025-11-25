ab8264033efa70a7ddd7aada3e78daa3bc3ab111
Nayan17107 <prajapatinayan17107@gmail.com>
2025-11-25 10:35:00 +0530

    25/11/25

---
+++ React-JS/API-redux-app/src/config/firebase.config.js
@@ -0,0 +1,12 @@
+// import { initializeApp } from "firebase/app";
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAV8qeszSC2GGViKbZhAJ3cJbMArW1aiYg",
+//     authDomain: "myntra-app-96868.firebaseapp.com",
+//     projectId: "myntra-app-96868",
+//     storageBucket: "myntra-app-96868.firebasestorage.app",
+//     messagingSenderId: "871977402734",
+//     appId: "1:871977402734:web:a216c06786334c04be017f"
+// };
+
+// const app = initializeApp(firebaseConfig);
