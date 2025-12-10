e99c1b0686badc3835388504c4fbc1c26c4a9984
Nayan17107 <prajapatinayan17107@gmail.com>
2025-12-10 16:59:43 +0530

    10/12/25

--- React-JS/Myntra/src/Store.js
+++ React-JS/Myntra/src/Store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productReducer from './Services/Reducers/ProductReducer';
-import { auth } from '../Config/firebase.config';
 
 export const store = configureStore({
     reducer: {
         products: productReducer,
-        auth: authReducer
     }
 });
